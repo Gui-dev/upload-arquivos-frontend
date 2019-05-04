6b55a91495433501aaebdff58158d8551489d2a0
Gui-dev <adrianon10@gmail.com>
2019-05-04 03:20:48 -0300

    Adicionando variaveis de ambiente da URL da API do heroku

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create( {
 
-  baseURL: ProcessingInstruction.env.REACT_APP_API_URL
+  baseURL: process.env.REACT_APP_API_URL
   // baseURL: 'http://192.168.0.105:3333'
 } )
 
